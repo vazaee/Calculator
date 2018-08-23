55c6a39ef556c3696538b9b7dcea500a19fca9df
GABRIEL VAZ DE SOUZA <17111238@pucrs.br>
2018-08-23 10:55:01 -0300

    calculadora atualização

--- src/main/java/o_cla/Calculator/Calculator.java
+++ src/main/java/o_cla/Calculator/Calculator.java
@@ -1,10 +1,42 @@
 package o_cla.Calculator;
 
 public class Calculator {
-	  public int evaluate(String expression) {
-	    int sum = 0;
-	    for (String summand: expression.split("\\+"))
-	      sum += Integer.valueOf(summand);
-	    return sum;
-	  }
+	public int evaluate(String expression) {
+		String tokens[] = expression.split("(?<=\\+)|(?=\\+)|(?<=\\-)|(?=\\-)");
+		
+		int primeiro = 0;
+		int result = 0;
+        
+		// Trata o sinal inicial
+		String aux = tokens[primeiro];
+		if (aux.equals("-")) {
+			System.out.println(aux);
+			primeiro++;
+			System.out.println(tokens[primeiro]);
+			result -= Integer.parseInt(tokens[primeiro]);
+			primeiro++;
+		}else if (aux.equals("+")) {
+			primeiro++;
+			result += Integer.parseInt(tokens[primeiro]);
+			primeiro++;
+		}else {
+			result += Integer.parseInt(aux);
+			primeiro++;
+		}
+		
+		// Trata a sequencia
+		for(int i=primeiro;i<tokens.length;i+=2) {
+			switch(tokens[i]) {
+			case "+":
+				result += Integer.parseInt(tokens[i+1]);
+				break;
+			case "-":
+				result -= Integer.parseInt(tokens[i+1]);
+				break;
+			}
+		}
+		
+		
+		return result;
+	}
 }
--- src/test/java/o_cla/Calculator/CalculatorTest.java
+++ src/test/java/o_cla/Calculator/CalculatorTest.java
@@ -2,27 +2,76 @@ package o_cla.Calculator;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
-	private Calculator c;
-	
-	@Before
-	public void inicializa() {
-		c = new Calculator();
-	}
-	
 	@Test
-	public void calcularVariasSomasTest() {
-		int resp = c.evaluate("5+3+2");
-		assertEquals(10,resp);
-	}
-	
-	@Test
-	public void calcularSoUmNumeroTest() {
-		int resp = c.evaluate("5");
-		assertEquals(5,resp);
+	public void testEvaluateSomas() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("3+8+5+9");
+		assertEquals(25, resp); //
 	}
 
+	@Test
+	public void testEvaluatePrimeiroNegativo() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("-3+8+5+9");
+		assertEquals(19, resp); //
+	}
+
+	@Test
+	public void testEvaluatePrimeiroPositivo() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("+3+8+5+9");
+		assertEquals(25, resp); //
+	}
+
+	@Test
+	public void testEvaluateSubtracoes() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("15-5-3-7-2");
+		assertEquals(-2, resp);
+	}
+	
+	@Test
+	public void testEvaluateMisto() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("3+8-5+4-6");
+		assertEquals(4, resp);
+	}
+	
+	@Test
+	public void testEvaluateSoUmaOperacao() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("3+8");
+		assertEquals(11, resp);
+	}
+	
+	@Test
+	public void testEvaluateResultadoNegativo() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("3-8");
+		assertEquals(-5, resp);
+	}
+	
+	@Test
+	public void testEvaluateSoUmNumero() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("3");
+		assertEquals(3, resp);
+	}
+
+	@Test
+	public void testEvaluateSoUmNumeroNegativo() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("-3");
+		assertEquals(-3, resp);
+	}
+
+	@Test
+	public void testEvaluateSoUmNumeroPositivo() {
+		Calculator c = new Calculator();
+		int resp = c.evaluate("3");
+		assertEquals(3, resp);
+	}
 }
